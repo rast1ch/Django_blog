ae080ff7c9ea37ffc959cb98c7772582ecc3f4a3
rast1ch <rast1ch2286@gmail.com>
2021-06-08 14:07:28 -0400

    Created Model and form for Comments

--- mysite/blog/forms.py
+++ mysite/blog/forms.py
@@ -1,5 +1,6 @@
 from django import forms
-
+from django.db.models import fields
+from .models import Comment
 
 class EmailPostForm(forms.Form):
     name = forms.CharField(max_length=25)
@@ -7,4 +8,8 @@ class EmailPostForm(forms.Form):
     to = forms.EmailField()
     comments = forms.CharField(required=False,widget=forms.Textarea)
 
-    
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('name', 'email','body')
+        
